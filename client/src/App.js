be763bfc050802381949769166be267881bdcf39
Chris Wallace <Chrisw1096@gmail.com>
2023-03-06 10:35:35 +1100

    api cal for open weather working, might need an await

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+
+import Home from './pages/homepage';
+import NotFound from "../src/pages/404NotFound"
+
+import Weather from './components/weather';
+export default function App() {
+  
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    fetchData();
+  }, [lat,long])
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+           
+           {(typeof data.main != 'undefined') ? (
+        <Weather weatherData={data}/>
+      ): (
+        <div></div>
+      )}
     </div>
   );
 }
+}
 
-export default App;
